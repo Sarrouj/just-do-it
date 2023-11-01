4168af2102df3233674fe117c43d022b62df9c53
Zaid <zaydssh500@gmail.com>
2023-11-01 16:29:47 +0100

    Updated

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
-btn = document.querySelector('.headerBtn');
-header = document.querySelector('.header');
-logo = document.querySelector('.logo');
-headerP = document.querySelector('.headerP');
-about = document.querySelector('.About');
-reset = document.querySelector('.Reset');
-addBtn = document.querySelector('.button');
+let btn = document.querySelector('.headerBtn');
+let header = document.querySelector('.header');
+let logo = document.querySelector('.logo');
+let headerP = document.querySelector('.headerP');
+let about = document.querySelector('.About');
+let reset = document.querySelector('.Reset');
+let addBtn = document.querySelector('.button');
+let taskTitle = document.querySelector('.taskTitle');
+let description = document.querySelector('.descriptionTextArea');
+let startDate = document.querySelector('.startDateInput');
+let endDate = document.querySelector('.endDateInput');
+
+
 
 
 // Animate Header
@@ -17,10 +23,48 @@ btn.addEventListener('click', ()=>{
     reset.classList.add('displayLinks');
 })
 
-// Add tasks area
-addBtn.onclick = function(){
-   let taskTitle = document.querySelector('.title');
-   let h4 = document.createElement('h4');
-   h4.textContent = 'Tasks';
-   taskTitle.appendChild(h4);
+// key to generate tasks header once
+let done = true;
+
+// generate tasks header
+function taskHeader(){
+    if(done){
+        done = false;
+        let taskTitle = document.querySelector('.title');
+        let h4 = document.createElement('h4');
+        h4.textContent = 'Tasks';
+        taskTitle.appendChild(h4);
+     
+         // filters
+         let filters = document.querySelector('.filters');
+         let liLow = document.createElement('li');
+         let liMiddle = document.createElement('li');
+         let liUrgent = document.createElement('li');
+         let liCompleted = document.createElement('li');
+         liLow.textContent = 'Low';
+         liMiddle.textContent = 'Middle';
+         liUrgent.textContent = 'Urgent';
+         liCompleted.textContent = 'Completed';
+         filters.appendChild(liLow);
+         filters.appendChild(liMiddle);
+         filters.appendChild(liUrgent);
+         filters.appendChild(liCompleted); 
+    }
 }
+
+
+// check if inputs is empty
+function checkInputsGenerate(){
+   let currentDate = new Date().toDateString();
+   console.log(currentDate)
+   console.log(startDate.value)
+}
+
+addBtn.onclick = function(){
+    // generate header Once
+    taskHeader();
+    checkInputsGenerate();
+}
+
+
+
